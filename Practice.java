0626fa94c710fc455572da93de55f07d56e767f5
Bindu5112005 <bindukrishnamurthyid@gmail.com>
2026-03-18 10:58:57 +0530

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,10 @@
+public class Practice{
+public static void main(String[] agrgs){
+int a=10;
+int b=20;
+int sum = a+b;
+System.out.println("The sum is"+sum);
+  System.out.println("hii");
+  System.out.println("hii bindu");
+}
+}
